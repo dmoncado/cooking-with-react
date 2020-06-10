e6c91eff673e1c3570534140c480fa4e23646fed
Derick Moncado <derickmoncado@gmail.com>
2020-06-10 15:59:35 -0700

    Added RecipeList and Recipe components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
-import Counter from "./Counter";
-import CounterHooks from "./CounterHooks";
-
-export const ThemeContext = React.createContext();
+import React from "react";
+import RecipeList from "./RecipeList";
 
 function App() {
-	const [theme, setTheme] = useState("red");
 	return (
-		<ThemeContext.Provider value={{ backgroundColor: theme }}>
-			Counter
-			<Counter initialCount={0} />
-			Counter Hooks
-			<CounterHooks initialCount={0} />
-			<button onClick={() => setTheme((prevTheme) => {
-					return prevTheme === "red" ? "blue" : "red";
-			})}>Toggle theme</button>
-		</ThemeContext.Provider>
+		<RecipeList recipes={sampleRecipes}/>
 	);
 }
+
+const sampleRecipes = [
+	{
+		id: 1,
+		name: 'Plain Chicken',
+		servings: 3,
+		cookTime: '1:45',
+		instructions: '1. Put salt on chicken\n 2. Put chicken in oven\n 3. Eat chicken'
+	},
+	{
+		id: 2,
+		name: 'Plain Pork',
+		servings: 5,
+		cookTime: '0:45',
+		instructions: '1. Put paprika on porn\n 2. Put pork in oven\n 3. Eat pork'
+	}
+]
 
 export default App;
